bd6c41ccafb080ca11a2d8ee50aa17e3cd125f18
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-26 18:27:56 -0300

    Create DiminuiLetras.java

---
+++ java/Aula 3/DiminuiLetras.java
@@ -0,0 +1,12 @@
+public class DiminuiLetras {
+    public void lowCase() {
+        // Implementação do método lowCase()
+        System.out.println("Método lowCase() executado com sucesso!");
+    }
+
+    public static void main(String[] args) {
+        DiminuiLetras ltpqn = new DiminuiLetras();
+
+        ltpqn.lowCase();
+    }
+}
