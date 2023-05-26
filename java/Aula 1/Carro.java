0b473d234b236f303a652cc2248858c9420fb766
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-26 18:33:11 -0300

    Create Carro.java

---
+++ java/Aula 1/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    //Construtor da classe carro
+
+    public Carro() {
+    //Construtor do objeto
+    }
+
+    public static void main(String[] args) {
+        //Construtor da classe carro
+
+        Carro ford = new Carro();
+    }
+}
