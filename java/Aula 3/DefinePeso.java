166fd76edb3b60bb43d84a86b0e9023ea14dfdd1
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-26 18:27:51 -0300

    Create DefinePeso.java

---
+++ java/Aula 3/DefinePeso.java
@@ -0,0 +1,9 @@
+public class DefinePeso {
+
+    public static void main(String[] args) {
+        String nome = "Eduarda";
+        int idade = 21;
+        double peso = 50.5;
+        System.out.printf("%s, %d anos, pesa %.2f kg!", nome, idade, peso);
+    }
+}
