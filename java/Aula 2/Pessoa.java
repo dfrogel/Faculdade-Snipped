d209fe366af91e8afc413c2a8406319cebcef826
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-26 18:33:33 -0300

    Create Pessoa.java

---
+++ java/Aula 2/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+    private int idade;
+    // os métodos get servem  para pegar alguma informação
+    public int getIdade() {
+        return idade;
+    }
+
+    //já os métodos sets servem para definir alguma informação
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
+
+
