9d9c1479fc9d835f17d0be9edb592b79a6c90e9e
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-30 20:10:15 -0300

    Create MontaVetor.java

---
+++ java/Aula 4/MontaVetor.java
@@ -0,0 +1,17 @@
+package java.Aula 4;
+
+public class MontaVetor {
+    public static void main(String[] args) {
+        int vet[] = new int [5];
+
+        vet[0] = 5;
+        vet[1] = 6;
+        vet[2] = 3;
+        vet[3] = 9;
+        vet[4] = 7;
+
+        for(int i = 0; i < vet.length; i++) {
+            System.out.println("Vetor na posição ["+ 1 +"]" + vet[i] );
+        }
+    }
+}
