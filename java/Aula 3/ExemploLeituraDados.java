862d15fc96d314316b1ee5693846d42ca6f4c66c
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-30 23:36:47 -0300

    Update ExemploLeituraDados.java

--- java/Aula 3/ExemploLeituraDados.java
+++ java/Aula 3/ExemploLeituraDados.java
@@ -2,17 +2,45 @@ import java.util.Scanner;
 
 public class ExemploLeituraDados {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Digite um valor inteiro (int):");
-        int entradaInt = scan.nextInt();
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Digite um valor inteiro (int): ");
+            int entradaInt = scan.nextInt();
+            scan.nextLine(); // Limpa o buffer de entrada
 
-        System.out.print("Digite um valor real (double):");
-        double entradaDouble = scan.nextDouble();
+            System.out.print("Digite um valor real (double): ");
+            double entradaDouble = scan.nextDouble();
+            scan.nextLine(); // Limpa o buffer de entrada
 
-        System.out.print("Digite um valor lógico (boolean):");
-        boolean entradaBoolean = scan.nextBoolean();
+            System.out.print("Digite um valor lógico (boolean): ");
+            boolean entradaBoolean = scan.nextBoolean();
+            scan.nextLine(); // Limpa o buffer de entrada
 
-        System.out.println("Digite uma string (palavra):");
-        String entradaString = scan.next();
+            System.out.print("Digite uma string (palavra): ");
+            String entradaString = scan.nextLine();
+
+            // Imprimindo os valores lidos
+            System.out.println("Valor inteiro: " + entradaInt);
+            System.out.println("Valor real: " + entradaDouble);
+            System.out.println("Valor lógico: " + entradaBoolean);
+            System.out.println("String: " + entradaString);
+        }
     }
 }
+
+
+
+/* EXPLICANDO O CÓDIGO
+ Neste código, utilizamos a classe Scanner para realizar a leitura de dados a partir da entrada do usuário.
+
+ No método main(), criamos uma instância de Scanner chamada scan e utilizamos métodos como nextInt(), 
+ nextDouble(), nextBoolean() e nextLine() para ler os diferentes tipos de dados.
+ 
+ No entanto, é necessário observar que ao ler tipos de dados diferentes em sequência (como int, double e boolean), 
+ pode ocorrer um problema conhecido como "quebra de linha" (ou "newline") no buffer de entrada. 
+ Isso acontece porque o método nextDouble(), por exemplo, não consome a quebra de linha deixada pelo nextInt(). 
+ Para resolver esse problema, utilizamos o método nextLine() imediatamente após a leitura de cada tipo 
+ de dado para limpar o buffer de entrada.
+
+ Após a leitura dos valores, imprimimos os mesmos para verificar se foram lidos corretamente.
+
+ */
