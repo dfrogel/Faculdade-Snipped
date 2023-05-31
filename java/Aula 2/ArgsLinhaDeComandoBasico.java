9cff897b0b7092bdcbca3829e7b28d2abcc1320b
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-30 23:36:22 -0300

    Update ArgsLinhaDeComandoBasico.java

--- java/Aula 2/ArgsLinhaDeComandoBasico.java
+++ java/Aula 2/ArgsLinhaDeComandoBasico.java
@@ -8,8 +8,19 @@ public class ArgsLinhaDeComandoBasico {
     }
 }
 
-/* explicando o código:
- * public static void main: é a assinatura do ponto de entrada da aplicação
- * String[]: Vetor de strings que permite a passagem de quantos argumentos quisermos
- * args: Parâmetro que recebe valores no momento da execução do código
+/* EXPLICANDO O CÓDIGO:
+
+ Neste exemplo, temos um programa simples em Java que demonstra o uso de argumentos da linha de comando.
+
+ O método "main" é o ponto de entrada da aplicação. Ele recebe um parâmetro "String[] args" que permite a 
+ passagem de argumentos quando o programa é executado na linha de comando.
+ 
+ No início do método, utilizamos "System.out.printf()" para exibir a quantidade de argumentos recebidos,
+ utilizando "args.length" para obter o tamanho do array de argumentos.
+ 
+ Em seguida, temos um loop "for" que percorre todos os elementos do array "args" e os imprime na saída padrão
+ utilizando novamente "System.out.printf()". Utilizamos "%n" para imprimir uma nova linha.
+ 
+ Dessa forma, ao executar o programa e passar argumentos na linha de comando, eles serão exibidos junto com
+ sua posição no array "args".
  */
