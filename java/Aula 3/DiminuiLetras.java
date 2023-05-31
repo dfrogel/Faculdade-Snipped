a3b84420aab4b4b30d2de35fdee5402d42624abd
Eduarda Frogel <dudafrogel23@gmail.com>
2023-05-30 23:36:43 -0300

    Update DiminuiLetras.java

--- java/Aula 3/DiminuiLetras.java
+++ java/Aula 3/DiminuiLetras.java
@@ -5,8 +5,21 @@ public class DiminuiLetras {
     }
 
     public static void main(String[] args) {
-        DiminuiLetras ltpqn = new DiminuiLetras();
+        // Criando uma instância da classe DiminuiLetras
+        DiminuiLetras diminuiLetras = new DiminuiLetras();
 
-        ltpqn.lowCase();
+        // Chamando o método lowCase() da instância criada
+        diminuiLetras.lowCase();
     }
 }
+
+/* EXPLICANDO O CÓDIGO
+ Neste código, temos uma classe chamada DiminuiLetras com um método chamado lowCase(). 
+ No método lowCase(), uma mensagem é impressa no console para indicar que o método foi executado com sucesso.
+
+ No método main(), é criada uma instância da classe DiminuiLetras usando o operador new, 
+ e essa instância é armazenada na variável diminuiLetras. 
+ Em seguida, o método lowCase() é chamado na instância criada, utilizando o operador de ponto (.) para acessar o método.
+
+ Ao executar o programa, a mensagem "Método lowCase() executado com sucesso!" será exibida no console.
+ */
