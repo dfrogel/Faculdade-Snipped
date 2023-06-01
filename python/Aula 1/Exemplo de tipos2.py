a619b385764bdf7c685370cf33ff2cc3b5dfeaa0
Eduarda Frogel <dudafrogel23@gmail.com>
2023-06-01 19:46:47 -0300

    Create Exemplo de tipos2.py

---
+++ python/Aula 1/Exemplo de tipos2.py
@@ -0,0 +1,14 @@
+# Exemplos de tipos de dados em Python
+
+# Inteiro
+valor_inteiro = 10
+# A variável 'valor_inteiro' armazena um número inteiro (int).
+
+# String
+valor_string = 'nome'
+# A variável 'valor_string' armazena um texto (string).
+
+# Ponto Flutuante
+valor_ponto_flutuante = 10.5
+# A variável 'valor_ponto_flutuante' armazena um número com casas decimais (float).
+
