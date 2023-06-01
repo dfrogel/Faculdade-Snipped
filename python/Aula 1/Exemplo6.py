3eb49213bc030b8b192948d1802cc71b47980a39
Eduarda Frogel <dudafrogel23@gmail.com>
2023-06-01 00:06:02 -0300

    Create Exemplo6.py

---
+++ python/Aula 1/Exemplo6.py
@@ -0,0 +1,9 @@
+# Utilizando o loop for para contar de 1 a 9
+for contagem in range(1, 10):
+    print(contagem)  # Imprime o valor da contagem atual
+
+# Utilizando o loop while para contar de 0 a 9
+contagem = 0  # Inicializa a variável contagem com o valor 0
+while contagem < 10:  # Executa o bloco de código enquanto a condição for verdadeira
+    print(contagem)  # Imprime o valor da contagem atual
+    contagem = contagem + 1  # Incrementa a variável contagem em 1 para a próxima iteração
