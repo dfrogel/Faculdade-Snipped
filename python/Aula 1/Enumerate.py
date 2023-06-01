13e2cfb461c8b647fddffa68ef2d32d588d00023
Eduarda Frogel <dudafrogel23@gmail.com>
2023-06-01 19:46:37 -0300

    Create Enumerate.py

---
+++ python/Aula 1/Enumerate.py
@@ -0,0 +1,7 @@
+nome = "Eduarda"
+
+# Itera sobre cada caractere da string 'nome' juntamente com o índice correspondente
+for i, c in enumerate(nome):
+    # Imprime a posição e o caractere na iteração atual
+    print("Posição {i}: {c}".format(i=i, c=c))
+
