d4ca66f5e74f481e2ba4b18a21f4059836231dd5
Eduarda Frogel <dudafrogel23@gmail.com>
2023-06-01 19:47:36 -0300

    Create Hello word.py

---
+++ python/Aula 1/Hello word.py
@@ -0,0 +1,8 @@
+nome = input("Digite um nome: ")  # Solicita ao usuário que digite um nome e armazena o valor na variável "nome"
+print("Olá %s, seja bem-vindo(a) à disciplina de Python. Parabéns pelo seu primeiro hello world!" % nome)
+# Imprime uma mensagem de boas-vindas junto com o nome fornecido pelo usuário utilizando formatação de string
+
+# Neste código em Python, a função input() é utilizada para receber a entrada do usuário, 
+# que é armazenada na variável nome. Em seguida, a função print() é usada para 
+# exibir uma mensagem de boas-vindas, utilizando a formatação de string % para inserir 
+# o valor da variável nome na mensagem.
